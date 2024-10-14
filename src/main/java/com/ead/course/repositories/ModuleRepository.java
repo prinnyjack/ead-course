1a126b0663a58169bd8302bd9154136348894371
Bruno <bruno-ams@outlook.com>
2024-10-14 10:49:06 -0300

    creating custom methods to delete modules and courses without using @CascadeType. Also implementing FetchType in our models and creating custom @Query

--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -1,9 +1,25 @@
 package com.ead.course.repositories;
 
 import com.ead.course.models.ModuleModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+
+    /* Dessa maneira eu consigo acessar o modulo que estou procurando e recebendo os dados tambem de qual curso
+    ele está inserido, mesmo utilizando  o FetchType Lazy, ele faz com que este metodo em especifico me retorne um
+     FetchType Eager do Course
+
+    @EntityGraph(attributePaths = "course")
+    ModuleModel findByTitle(String title);
+     */
+    @Query(value = "select * from tb_modules where course_course_id = :courseId", nativeQuery = true)
+    List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
+
+
 }
